0a109db62aac79b74c1319c9ac8d7e036e0ca578
Jewel <89004717+pentagonmay@users.noreply.github.com>
2025-10-07 23:41:25 -0700

    Update flask.py

--- python_files/flask.py
+++ python_files/flask.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('HomePage.html')  # Renders a basic HTML page
+
+if __name__ == '__main__':
+    app.run(debug=True)
