d41321a6b5f3f231c71389e619603c765b485ed7
Shubnoor Singh <shubnoor05@gmail.com>
2025-10-08 21:16:05 -0700

    Add files via upload

---
+++ javascript_files/ReadAloud.js
@@ -0,0 +1,11 @@
+function readAloud(text) {
+  if ('speechSynthesis' in window) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.rate = 0.8; // Slower for seniors
+    utterance.pitch = 1.0;
+    utterance.volume = 1.0;
+    window.speechSynthesis.speak(utterance);
+  } else {
+    alert('Read aloud is not supported in your browser. Please call our helpline for assistance.');
+  }
+}
