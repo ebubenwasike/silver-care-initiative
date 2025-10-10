d3506d511bf83b1155f116281d92755e3b3a3a62
Priya <priya1592003@gmail.com>
2025-10-09 22:47:36 -0700

    Add MySQL connection and endpoint to list tables
    Introduced MySQL database connection using mysql.connector and added a new '/getTable' GET endpoint to return the list of tables in the database. Also updated the app entry point and removed the previous home route.

--- python_files/flask.py
+++ python_files/flask.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
-
+import mysql.connector
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('HomePage.html')  # Renders a basic HTML page
+con=mysql.connector.connect(
+    host='project275',
+    user='project275',
+    password='projecy275',
+    database='mydatabse'
+)
 
-if __name__ == '__main__':
+@app.route('/getTable',methods=['GET'])
+def get_tables():
+    cursor=con.cursoe()
+    cursor.execute("SHOW TABLES;")
+    tables=cursor.fetchall()
+    cursor.close()
+    con.close()
+    table_names=[table[0] for table in tables]
+    return jsonify({"tables":table_names}),200
+
+if  __name__==" main ":
+    print("connecting to database")
     app.run(debug=True)
